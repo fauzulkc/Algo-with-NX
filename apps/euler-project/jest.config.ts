75f9d6f8d2f715e6e1aef36a2924b17ecd6346b1
Fauzul Chowdhury <fauzulkc@gmail.com>
2024-05-14 14:49:27 -0400

    feat: ✅ Added new test to test against 1000, and moved project from generic to euler-project

--- apps/euler-project/jest.config.ts
+++ apps/euler-project/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'generic',
+  displayName: 'euler-project',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/generic',
+  coverageDirectory: '../../coverage/apps/euler-project',
 };
