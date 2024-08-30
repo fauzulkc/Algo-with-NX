0cb97c68bb401926899562f49ca06e3391b0517c
Fauzul Chowdhury <fauzulkc@gmail.com>
2024-08-31 02:25:53 +0600

    fix: removed redundant number divide by factor in largeest prime factor check problem

--- apps/euler-project/src/problems/largest-prime-factor.ts
+++ apps/euler-project/src/problems/largest-prime-factor.ts
@@ -1,11 +1,12 @@
-export const checkLargestPrimeFactor = (number: number): number => {
+export const checkLargestPrimeFactor = ( number: number ): number =>
+{
   let factor = 2;
   let lastFactor = 1;
-  while (number > 1) {
-    if (number % factor === 0) {
+  while ( number > 1 ) {
+    if ( number % factor === 0 ) {
       lastFactor = factor;
-      number /= factor;
-      while (number % factor === 0) {
+      // number /= factor;
+      while ( number % factor === 0 ) {
         number /= factor;
       }
     }
