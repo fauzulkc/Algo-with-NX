d8158e2f7eb3738f1b3aa795300384191825f73b
Fauzul Chowdhury <fauzulkc@gmail.com>
2024-08-31 02:39:11 +0600

    feat: added twoSum Leetcode problem solution and test

---
+++ apps/leet-code/src/problems/two-sum.ts
@@ -0,0 +1,11 @@
+export function twoSum ( nums: number[], target: number ): number[]
+{
+    for ( let i = 0; i < nums.length; i++ ) {
+        for ( let j = i + 1; j < nums.length; j++ ) {
+            if ( target === nums[ i ] + nums[ j ] ) {
+                return [ i, j ];
+            }
+        }
+
+    }
+};
