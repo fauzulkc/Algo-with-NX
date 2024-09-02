0fe3a81e7bfebbe28dc5e29df3fc05585157c33b
Fauzul Chowdhury <fauzulkc@gmail.com>
2024-09-03 05:21:36 +0600

    doc: requires improvement on twoSum problem

--- apps/leet-code/src/problems/two-sum.ts
+++ apps/leet-code/src/problems/two-sum.ts
@@ -1,5 +1,6 @@
 export function twoSum ( nums: number[], target: number ): number[]
 {
+    // BAD O(n2) solution needs to be fixed
     for ( let i = 0; i < nums.length; i++ ) {
         for ( let j = i + 1; j < nums.length; j++ ) {
             if ( target === nums[ i ] + nums[ j ] ) {
