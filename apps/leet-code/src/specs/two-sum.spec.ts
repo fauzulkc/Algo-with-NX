2cb90799b8d01166d5bc1e10b06689fb82f3f4b4
Fauzul Chowdhury <fauzulkc@gmail.com>
2024-09-03 07:55:58 +0600

    fix: fixed the solution for twoSum to be more efficient and hashbased

--- apps/leet-code/src/specs/two-sum.spec.ts
+++ apps/leet-code/src/specs/two-sum.spec.ts
@@ -1,5 +1,5 @@
-import { twoSum } from "../problems/two-sum";
-describe( "Two Sum", () =>
+import { twoSum, twoSumON2 } from "../problems/two-sum";
+describe( "Two Sum with HashMap Solution", () =>
 {
     it( "should check if sum of two numbers in an array equals to the target", () =>
     {
@@ -8,5 +8,16 @@ describe( "Two Sum", () =>
     it( "should check if sum of two numbers in an array equals to the target", () =>
     {
         expect( twoSum( [ 3, 2, 4 ], 6 ) ).toStrictEqual( [ 1, 2 ] );
+    } );
+} );
+describe( "Two Sum with O(n2) Solution", () =>
+{
+    it( "should check if sum of two numbers in an array equals to the target", () =>
+    {
+        expect( twoSumON2( [ 2, 7, 11, 15 ], 9 ) ).toStrictEqual( [ 0, 1 ] );
+    } );
+    it( "should check if sum of two numbers in an array equals to the target", () =>
+    {
+        expect( twoSumON2( [ 3, 2, 4 ], 6 ) ).toStrictEqual( [ 1, 2 ] );
     } );
 } );
