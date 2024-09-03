38fcd03040c2f7f7a035d9df65557759012b8a33
Fauzul Kabir Chowdhury <fauzulkc@gmail.com>
2024-09-03 07:59:24 +0600

    twoSum (#19)
    * feat: added twoSum Leetcode problem solution and test
    
    * feat: prefix-sum-range added to leetcode problems along with test case
    
    * feat: add two numbers linked list input and out problem add with test case
    
    * readme changed
    
    * feat: added longest substring without repeat problem and test case
    
    * feat: Added 1D array to 2D array probem with test and two different approaches
    
    * feat: Added Contains Duplicate problem along with 3 test cases
    
    * feat: added valid anagram solution in two ways hashmap and slicing resepectively, and added test cases against them
    
    * doc: requires improvement on twoSum problem
    
    * fix: fixed the solution for twoSum to be more efficient and hashbased
    
    * conflict fix

--- apps/leet-code/src/problems/two-sum.ts
+++ apps/leet-code/src/problems/two-sum.ts
@@ -1,5 +1,19 @@
 export function twoSum ( nums: number[], target: number ): number[]
 {
+    const seenNumbers = new Map();
+    for ( let i = 0; i < nums.length; i++ ) {
+        let diff = target - nums[ i ];
+        if ( seenNumbers.get( nums[ i ] ) !== undefined ) {
+            return [ seenNumbers.get( nums[ i ] ), i ];
+        }
+        seenNumbers.set( diff, i );
+    }
+    return [];
+
+};
+
+export function twoSumON2 ( nums: number[], target: number ): number[]
+{
     // BAD O(n2) solution needs to be fixed
     for ( let i = 0; i < nums.length; i++ ) {
         for ( let j = i + 1; j < nums.length; j++ ) {
@@ -7,6 +21,6 @@ export function twoSum ( nums: number[], target: number ): number[]
                 return [ i, j ];
             }
         }
-
     }
-};
+    return [];
+}
--- apps/leet-code/src/specs/two-sum.spec.ts
+++ apps/leet-code/src/specs/two-sum.spec.ts
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
+import { twoSum, twoSumON2 } from "../problems/two-sum";
+describe( "Two Sum with HashMap Solution", () =>
+=======
 import { twoSum } from "../problems/two-sum";
 describe( "Two Sum", () =>
+>>>>>>> origin/main
 {
     it( "should check if sum of two numbers in an array equals to the target", () =>
     {
@@ -9,4 +14,18 @@ describe( "Two Sum", () =>
     {
         expect( twoSum( [ 3, 2, 4 ], 6 ) ).toStrictEqual( [ 1, 2 ] );
     } );
+<<<<<<< HEAD
+} );
+describe( "Two Sum with O(n2) Solution", () =>
+{
+    it( "should check if sum of two numbers in an array equals to the target", () =>
+    {
+        expect( twoSumON2( [ 2, 7, 11, 15 ], 9 ) ).toStrictEqual( [ 0, 1 ] );
+    } );
+    it( "should check if sum of two numbers in an array equals to the target", () =>
+    {
+        expect( twoSumON2( [ 3, 2, 4 ], 6 ) ).toStrictEqual( [ 1, 2 ] );
+    } );
+=======
+>>>>>>> origin/main
 } );
